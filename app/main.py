b6df9b32a1cd2f8ee701f70d643eb26f77437fdc
Jacob-Armstrong <silverlock99@gmail.com>
2025-05-20 13:58:48 -0700

    Create models file, refactor db creation to a function

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from .database import engine
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
 
 app = FastAPI(
     title="Portfolio API",
     version="1.0.0",
     description="Welcome to my portfolio!"
 )
-
-
 
 @app.get("/")
 async def root():
@@ -16,3 +19,6 @@ async def root():
         "Using": "FastAPI, Pydantic, SQLModel and PostgreSQL",
         "Documentation": ""
     }
+
+if __name__ == "__main__":
+    create_db()
