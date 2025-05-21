df59360a3b796d6d9ac26d10cde49c1d71fedebf
Jacob-Armstrong <silverlock99@gmail.com>
2025-05-21 12:55:19 -0700

    Remove old SQLModel equivalents

--- app/models.py
+++ app/models.py
@@ -45,11 +45,3 @@ class Visits(Base):
     relation = Column(String, nullable=False)
     message = Column(String, nullable=False)
     date = Column(DateTime(timezone=True), server_default=func.now())
-
-# class Skills(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     category: str
-#     name: str
-
-# class Education(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
